83a8375d6820f03aaa845eacd1c76fde1c3f14e6
Kyle Martinez <elyk17@yahoo.com>
2022-06-16 16:46:18 -0600

    added injection

--- TestBase/WebDriverSetup.py
+++ TestBase/WebDriverSetup.py
@@ -19,27 +19,13 @@ class WebDriverSetup(unittest.TestCase):
         # self.driver = webdriver.Firefox(executable_path=r'C:\SeleniumDrivers\\geckodriver.exe', options=options)
         self.driver = webdriver.Chrome(executable_path='C:\SeleniumDrivers\\chromedriver.exe')
         self.driver.maximize_window()
-        execu = '''
-        var scr = document.createElement('script');
-        scr.type =  'text/javascript';
-        scr.src = "https://go.playerzero.ai/record/6274691b00fbad01561df689"
-        document.head.appendChild(scr)
-        '''
-        try:
-            self.driver.execute_script(execu)
-            time.sleep(30)
-
-        except Exception as e:
-            print(e)
+        self.driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': 'https://go.playerzero.ai/record/6274691b00fbad01561df689'})
 
 
 
     def tearDown(self):
         if(self.driver != None):
             
-            for string in self.driver.get_log('session'):
-                print(string)
-            print("Cleanup of test environment")
             self.driver.close()
             self.driver.quit()
 
